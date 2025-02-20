0e0f2ba78186c7405cdd942bb093a4c4dcb2f0ef
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2025-02-20 08:25:54 -0500

    utilización de variables de entorno

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config();
 const express=module.require("express");
 const conexionSequelize = require("./sequelize/");
 const app= express();
--- backend/sequelize/index.js
+++ backend/sequelize/index.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');	
 //cadena de conexión
 const conexionSequelize = new Sequelize({
-    host: '127.0.0.1',
-    username: 'postgres',
-    password: 'admin',
-    database: 'plan_cuenta',
-    port: '5432',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
     logging:false,
     define: {
